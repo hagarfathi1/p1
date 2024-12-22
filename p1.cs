3790e85f27bfdd01a3e3913a01a8fdc5dc75cafe
hagarfathi1 <140267235+hagarfathi1@users.noreply.github.com>
2024-12-23 00:17:18 +0200

    Add files via upload

---
+++ p1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    // Heap-Sort Algorithms
+    public static void HeapSort(int[] array)
+    {
+        int n = array.Length;
+
+        for (int i = n / 2 - 1; i >= 0; i--)
+            Heapify(array, n, i);
+
+        for (int i = n - 1; i > 0; i--)
+        {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            Heapify(array, i, 0);
+        }
+    }
+
+    private static void Heapify(int[] array, int n, int i)
+    {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && array[left] > array[largest])
+            largest = left;
+
+        if (right < n && array[right] > array[largest])
+            largest = right;
+
+        if (largest != i)
+        {
+            int swap = array[i];
+            array[i] = array[largest];
+            array[largest] = swap;
+
+            Heapify(array, n, largest);
+        }
+    }
+  //building the max heap :o(n)
+  //for each node :o(logn)
+  //sorting:o(nlogn)
+  //result:o(nlogn)
---
+++ p2.cs
@@ -0,0 +1,57 @@
+﻿// Kruskal's Algorithm
+public class Edge : IComparable<Edge>
+{
+    public int Source, Destination, Weight;
+
+    public int CompareTo(Edge other)
+    {
+        return this.Weight.CompareTo(other.Weight);
+    }
+}
+
+public class Kruskal
+{
+    private int[] parent;
+
+    public List<Edge> FindMST(int vertices, List<Edge> edges)
+    {
+        edges.Sort();
+        parent = new int[vertices];
+        for (int i = 0; i < vertices; i++)
+            parent[i] = i;
+
+        List<Edge> mst = new List<Edge>();
+
+        foreach (var edge in edges)
+        {
+            int root1 = Find(edge.Source);
+            int root2 = Find(edge.Destination);
+
+            if (root1 != root2)
+            {
+                mst.Add(edge);
+                Union(root1, root2);
+            }
+        }
+
+        return mst;
+    }
+
+    private int Find(int vertex)
+    {
+        if (parent[vertex] != vertex)
+            parent[vertex] = Find(parent[vertex]);
+
+        return parent[vertex];
+    }
+
+    private void Union(int root1, int root2)
+    {
+        parent[root2] = root1;
+    }
+}
+
+//sorting 0(ElogE)
+
+
+
